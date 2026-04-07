cd9bf783cc74082af7d275cc0b8d8321e8eae3d2
alicodes86 <ahmed.ali17@dcmail.ca>
2026-04-07 11:20:14 -0400

    Update java.js
    Added text strings

--- week10/part1/java/java.js
+++ week10/part1/java/java.js
@@ -13,27 +13,34 @@ function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-
 // Raw text strings
-
-
-
 // Willy the Goblin
 // Big Daddy
 // Father Christmas
-
 // the soup kitchen
 // Disneyland
 // the White House
 
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the Whitw House"];
+
 // spontaneously combusted
 // melted into a puddle on the sidewalk
 // turned into a slug and slithered away
+const events = [
+	"spontaneously combusted",
+	"melted into a puddle on the sidewalk",
+	"turned into a slug and slithered away",
+	];
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+	
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk.
+  When they got to :inserty:, they stared in horror for a few moments,
+  then :insertz:. Bob saw the whole thing,
+  but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
   return storyText;
 }
