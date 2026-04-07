66abbd1ff4334ec728c4d527c15ccf74b167e254
alicodes86 <ahmed.ali17@dcmail.ca>
2026-04-07 12:50:36 -0400

    final steps
    checked the files with validator and made changes to html and java files. fixed the errors.

--- week10/part1/java/java.js
+++ week10/part1/java/java.js
@@ -3,7 +3,7 @@
    Date: Apr 7, 2026
    Week 11 assignment Story Generator Part 1 */
 
-// Complete variable definitions and random functions
+// Complete variable definitions and random function
 
 const customName = document.getElementById("custom-name");
 const generateBtn = document.querySelector(".generate");
@@ -13,57 +13,48 @@ function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-// Raw text strings
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-// the soup kitchen
-// Disneyland
-// the White House
+
+// Solution: Raw text strings
 
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const places = ["the soup kitchen", "Disneyland", "the Whitw House"];
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+const places = ["the soup kitchen", "Disneyland", "the White House"];
 const events = [
-	"spontaneously combusted",
-	"melted into a puddle on the sidewalk",
-	"turned into a slug and slithered away",
-	];
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away",
+];
 
-// Partial return random string function
+// Solution: Partial return random string function
 
 function returnRandomStoryString() {
-	const randomCCharacter = randomValueFromArray(characters);
-	const randomPlace = randomValueFromArray(places);
-	const randomEvent = randomValueFromArray(events);
-	
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk.
-  let storyText = 'It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk.
-  When they got to ${randomPlace}, they stared in horror for a few moments,
-  then %{randomEvent}. Bob saw the whole thing,
-  but was not surprised — ${tandomCharacter} weighs 300 pounds, and it was a hot day. ';
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+
+  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
 
   return storyText;
 }
 
-// Event listener and partial generate function definition
+// Solution: Event listener and partial generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString();
+
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
