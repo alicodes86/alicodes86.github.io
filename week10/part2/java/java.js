ffbc851fa0490bc0c0079954849d1322a61418bd
alicodes86 <ahmed.ali17@dcmail.ca>
2026-04-07 21:57:54 -0400

    Update java.js
    created loop through the images

--- week10/part2/java/java.js
+++ week10/part2/java/java.js
@@ -22,3 +22,30 @@ const images = [
 ];
 	{ filename: "pic5.jpg", alt: "Large moth on a leaf" },
 ];
+// Solution: Loop through the images
+
+// Create a baseURL constant containing the baseURL of the images
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+// Loop through the images using a for...of loop
+for (const image of images) {
+  // Create a new image element
+  const newImage = document.createElement("img");
+  // Set the source and alt text for the image
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  // Make the image focusable via the keyboard
+  newImage.tabIndex = "0";
+  // Append the image as a child of the thumbBar
+  thumbBar.appendChild(newImage);
+  // Update the display to show the image full size when a thumb is clicked
+  newImage.addEventListener("click", updateDisplayedImage);
+  // Update the display to show the image full size when the "Enter" key
+  // is pressed after it has been focused
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
