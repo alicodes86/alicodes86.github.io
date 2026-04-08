97b8948144bdaf84afcd28462359b894a8000a54
alicodes86 <ahmed.ali17@dcmail.ca>
2026-04-08 10:38:56 -0400

    Update main.js
    added event listener

--- week10/part3/java/main.js
+++ week10/part3/java/main.js
@@ -41,3 +41,19 @@ class Ball {
   }
   // …
 }
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
