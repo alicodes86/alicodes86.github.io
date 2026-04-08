6112992df7fc415cde0cfb318c8e69b9323e49c6
alicodes86 <ahmed.ali17@dcmail.ca>
2026-04-07 21:47:06 -0400

    Update java.js
    added commit to java file, created data objects

--- week10/part2/java/java.js
+++ week10/part2/java/java.js
@@ -1,4 +1,9 @@
+/* Name: Ahmed alig
+   File: java.js
+   Date: Apr 7, 2026
+   Week 11 assignment image gallery Part 2 */
 
+// Complete variable definitions and random function
 
 
 const displayedImage = document.querySelector(".displayed-img");
@@ -6,3 +11,14 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Solution: Create a data object
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+	{ filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
